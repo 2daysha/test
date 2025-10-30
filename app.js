1cc2c2a6aca75a361f189dfc6fb5327311f88820
2daysha <vinaevadara@gmail.com>
2025-10-30 13:24:25 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -191,7 +191,7 @@ class LoyaltyProApp {
 
         const nav = document.querySelector('.bottom-nav');
         if (nav) nav.style.display = 'none';
-        
+
         this.setAuthState('unauthenticated');
     }
 
@@ -377,9 +377,7 @@ class LoyaltyProApp {
         container.innerHTML = `
             <div class="search-container">
                 <div class="search-box">
-                    <svg class="search-icon" viewBox="0 0 24 24" fill="currentColor">
-                        <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-                    </svg>
+                    <img src="icons/search.svg" alt="Поиск" class="search-icon">
                     <input type="text" class="search-input" id="search-input" placeholder="Поиск товаров...">
                     <button class="search-clear" id="search-clear" style="display: none;">×</button>
                 </div>
